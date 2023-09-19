504c6dd71e99e6762dbdc82d53e4670f987e0b9f
Yahir Ponce <142949341+YahirPonce24@users.noreply.github.com>
2023-09-19 17:48:50 -0600

    Add files via upload

---
+++ EVA1_11_TIPOS_DATOS/src/eva1_11_tipos_datos/EVA1_11_TIPOS_DATOS.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_11_tipos_datos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Legion
+ */
+public class EVA1_11_TIPOS_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        
+        byte califa; // el byte va de -128...127
+        califa = 127 ;
+        //califa = 200 ;
+        //califa = -128;
+        //califa = -200;
+        //califa = 127+1;
+/*        Scanner imput = new Scanner(System.in);
+        System.out.println("Sumar valor:");
+        byte captu = imput.nextByte();
+        System.out.println(califa + captu);         */
+
+        int valor;
+        valor = Integer.MAX_VALUE;
+            System.out.println(valor);
+        valor = Integer.MIN_VALUE;
+            System.out.println(valor);
+        
+        char caracter= 'ñ';
+        System.out.println(caracter);
+        System.out.println(caracter + 0);
+        
+       
+        
+        
+        
+        
+    }
+    
+}
